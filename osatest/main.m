3e6145d0f86e287da15e4a06ae144a11d4c27973
hhas <hhas@users.sourceforge.net>
2016-01-24 16:26:23 +0000

    Initial Commit

---
+++ osatest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  osatest
+//
+//  Created by Hamish Sanderson on 24/01/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
